c4fb10bd4d51a20058d4c8a78076449c7d51c452
GarlicBuns19 <planterx26@gmail.com>
2022-06-04 16:41:10 +0100

    var let const

--- scripts/code.js
+++ scripts/code.js
@@ -32,5 +32,22 @@ var ourName;
 /* Write variable names in JavaScript in camelCase. In camelCase, 
 multi-word variable names have the first word in lowercase, second word in caps */
     var someVariable;
-    var anotherVariableName;
+    var anotherVariableName;z
     var thisVariableNameIsSoLong;
+
+// One of the biggest problems with declaring variables with the var keyword is that you can easily overwrite variable declarations:
+
+    var camper = "James";
+    var camper = "David";
+    console.log(camper);
+
+/* In the code above, the camper variable is originally declared as James,
+   and is then overridden to be David. The console then displays the string David. */
+
+// Use let, a variable with the same name can only be declared once.
+
+/*const is like let with the added bonus that variables declared using const are read-only.
+  Constant value, which means that once a variable is assigned with const, it cannot be reassigned*/
+    const FAV_PET = "Cats";
+    FAV_PET = "Dogs";
+    // The console will display an error due to reassigning the value of FAV_PET.
