7d68bc8611e5d1a6c93e46fdd77b61eebdf7c0a8
GarlicBuns19 <planterx26@gmail.com>
2022-06-04 16:34:01 +0100

    Added info

--- scripts/code.js
+++ scripts/code.js
@@ -2,3 +2,35 @@
 
 /* This is a
 multi-line comment */
+
+// JavaScript Variables
+/*  undefined
+    null
+    boolean
+    string '12' or "12"
+    symbol
+    bigint
+    number 12
+    object*/
+
+// We tell JavaScript to create or declare a variable by putting the keyword var in front of it
+var ourName;
+/*  creates a variable called ourName. 
+    Variable names can be made up of numbers, letters, and $ or _, 
+    but may not contain spaces or start with a number.  */
+
+// The operator equal sign = stores a value
+    var myVar;
+    myVar = 5;
+
+    var myVar = 0;
+
+// A string
+    var myName = "your name";
+
+// JavaScript all variables and function names are case sensitive. This means that capitalization matters.
+/* Write variable names in JavaScript in camelCase. In camelCase, 
+multi-word variable names have the first word in lowercase, second word in caps */
+    var someVariable;
+    var anotherVariableName;
+    var thisVariableNameIsSoLong;
