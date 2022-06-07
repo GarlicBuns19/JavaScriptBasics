9e0ff68cd38c278df7778ace092e699ee9046a6c
GarlicBuns19 <planterx26@gmail.com>
2022-06-07 19:01:30 +0100

    global functions

--- scripts/code.js
+++ scripts/code.js
@@ -373,3 +373,10 @@ multi-word variable names have the first word in lowercase, second word in caps 
         return num*5
       }
       
+    //   Global Scope and Functions
+    //   In JavaScript, scope refers to the visibility of variables. Variables which are defined outside of a function block have Global scope. 
+    // This means, they can be seen everywhere in your JavaScript code.
+      
+    //   Variables which are declared without the let or const keywords are automatically created in the global scope. 
+    // This can create unintended consequences elsewhere in your code or when running a function again. 
+    // You should always declare your variables with let or const.
